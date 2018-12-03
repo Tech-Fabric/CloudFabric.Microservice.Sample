846e8be7d28eeabe98a97da2063d6bb15d47cb19
Preetham Reddy <preetham@techfabric.co>
2018-12-02 18:37:08 -0700

    Adding Status Codes for Controller

--- CloudFabric.Sample/CloudFabric.SampleService/Controllers/ProductController.cs
+++ CloudFabric.Sample/CloudFabric.SampleService/Controllers/ProductController.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using CloudFabric.SampleService.DTOs;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using CloudFabric.SampleService.DTOs;
 
 namespace CloudFabric.SampleService.Controllers
 {
@@ -12,39 +11,110 @@ namespace CloudFabric.SampleService.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        public ILogger<ProductController> _logger { get; set; }
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var returnObject = new string[] { "value1", "value2" };
+            return Ok(returnObject);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [ProducesResponseType(200, Type = typeof(ProductDTO))]
+        [ProducesResponseType(404)]
+        public IActionResult GetById(int id)
         {
-            return "value";
+            try
+            {
+                var product = new ProductDTO(); // This needs to be fetched using an instance of IProductRepository. For ex, _repository.GetProduct(id)
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message + " " + ex.StackTrace);
+                return BadRequest("Message");
+            }
         }
 
-        [HttpPost("")]
-        public async Task<ActionResult<ProductDTO>> AddNewProduct([FromBody] ProductDTO newProduct)
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(ProductDTO))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> CreateAsync([FromBody] ProductDTO product)
         {
-            await Task.Delay(1000);
-            if (!ModelState.IsValid)
+            try
             {
-                return new BadRequestResult();
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var newProduct = new ProductDTO(); //await _repository.AddProductAsync(product);
+
+                return CreatedAtAction(nameof(GetById), new { id = newProduct.Id }, newProduct);
             }
-            return new ObjectResult(newProduct);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message + " " + ex.StackTrace);
+                return BadRequest("Message");
+            }
         }
 
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Update(long id, ProductDTO product)
         {
+            try
+            {
+                var newProduct = new ProductDTO(); //await _repository.AddProductAsync(product);
+                if (newProduct == null)
+                {
+                    return NotFound();
+                }
+
+                newProduct.Name = "newName";
+                newProduct.Category = "newCategory";
+
+                // Save Product
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message + " " + ex.StackTrace);
+                return BadRequest("Message");
+            }
         }
 
-
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(long id)
         {
+            try
+            {
+                var newProduct = new ProductDTO(); //await _repository.AddProductAsync(product);
+                if (newProduct == null)
+                {
+                    return NotFound();
+                }
+
+                // Delete Object and Save Changes.
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message + " " + ex.StackTrace);
+                return BadRequest("Message");
+            }
         }
     }
 }
--- CloudFabric.Sample/CloudFabric.SampleService/DTOs/ProductDTO.cs
+++ CloudFabric.Sample/CloudFabric.SampleService/DTOs/ProductDTO.cs
@@ -7,6 +7,7 @@ namespace CloudFabric.SampleService.DTOs
 {
     public class ProductDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
         public string Subcategory { get; set; }
